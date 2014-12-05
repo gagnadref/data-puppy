c9f5c64bf959096d9a3299a7eda94106820e6d38
fgagnadre <florian.gagnadre@student.ecp.fr>
2014-12-05 19:56:11 +0100

    Changing code organization

--- datapuppy/HTTPLog.py
+++ datapuppy/HTTPLog.py
@@ -12,7 +12,24 @@ class HTTPLog:
 		self.referer = referer
 		self.agent = agent
 
+	def getURL(self):
+		try:
+			return self.request.split()[1]
+		except IndexOutOfBOund:
+			raise ValueError("Cannot find any URL in log")
+
+	def getSection(self):
+		regex = r"^\/[^\/]*"
+		matchObj = re.match(regex,self.getURL())
+		if matchObj:
+			return matchObj.group(0)
+		else:
+			raise ValueError("Cannot find any section in URL")
+
 class Parser:
+	"""
+	The parser parses a w3c-formatted HTTP access log into a HTTPLog object
+	"""
 	ip = "(\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3})?"
 	client = "(\\S+)"
 	user = "(\\S+)"
@@ -22,7 +39,7 @@ class Parser:
 	bytes = "(\\S+)"
 	referer = "\"(.*?)\""
 	agent = "\"(.*?)\""
-	regex = r"%s %s %s %s %s %s %s %s %s" %(ip, client, user, dateTime, request, status, bytes, referer, agent)
+	regex = "%s %s %s %s %s %s %s %s %s" %(ip, client, user, dateTime, request, status, bytes, referer, agent)
 
 	def parse(cls,string):
 		matchObj = re.match(cls.regex, string)
---
+++ test/TestHTTPLog.py
@@ -0,0 +1,30 @@
+from datapuppy import HTTPLog
+import unittest
+
+class TestHTTPLog(unittest.TestCase):
+    def test_parse(self):
+        log = HTTPLog.Parser.parse("82.67.154.104 - - [05/Dec/2014:14:01:02 +0200] \"GET /pages/create HTTP/1.1\" 200 181 \"-\" \"-\"")
+        self.assertEqual(log.ip, "82.67.154.104")
+        self.assertEqual(log.client, "-")
+        self.assertEqual(log.user, "-")
+        self.assertEqual(log.dateTime, "[05/Dec/2014:14:01:02 +0200]")
+        self.assertEqual(log.request, "GET /pages/create HTTP/1.1")
+        self.assertEqual(log.status, 200)
+        self.assertEqual(log.bytes, 181)
+        self.assertEqual(log.referer, "-")
+        self.assertEqual(log.agent, "-")
+
+        self.assertRaises(ValueError,HTTPLog.Parser.parse,"82.67.154.104 - - \"GET /pages/create HTTP/1.1\" 200 181 \"-\" \"-\"")
+
+    def test_getURL(self):
+        log = HTTPLog.Parser.parse("82.67.154.104 - - [05/Dec/2014:14:01:02 +0200] \"GET /pages/create HTTP/1.1\" 200 181 \"-\" \"-\"")
+        self.assertEqual(log.getURL(), "/pages/create")
+
+    def test_getSection(self):
+        log = HTTPLog.Parser.parse("82.67.154.104 - - [05/Dec/2014:14:01:02 +0200] \"GET /pages/create HTTP/1.1\" 200 181 \"-\" \"-\"")
+        self.assertEqual(log.getSection(), "/pages")
+
+
+if __name__ == '__main__':
+    unittest.main()
+
